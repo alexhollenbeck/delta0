d8e928cbced4b6c4f1e03551851358360f969114
Alex Hollenbeck <alexhollenbeck@gmail.com>
2014-05-09 10:00:43 -0500

    Added jquery library.

--- js/homepageHandlers.js
+++ js/homepageHandlers.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
 	$('.toggle-box').click(function() {
 		toggleBox();
-	})
+	});
+	$('.toggle-box-2').click(function() {
+		toggleBox2();
+	});
 });
 
 function toggleBox() {
 	$('.toggle-box').toggleClass('color1 color2');
+}
+
+function toggleBox2() {
+	$('toggle-box-2').toggleClass('color1 color3');
 }
--- tabview/handlers.js
+++ tabview/handlers.js
@@ -2,9 +2,45 @@ $(document).ready(function() {
   console.log('ready');
   $('.toggle-box').click(function() {
     toggleBox();
+  });
+  $('#page-tab').click(function() {
+  	pageTabClicked();
   })
 });
 
 function toggleBox() {
-  $('.toggle-box').toggleClass('color1 color2');
+	if ($('.toggle-box').hasClass('color-scheme-1')) {
+		if ($('.toggle-box').hasClass('color1')) {
+			$('.toggle-box').addClass('color2');
+			$('.toggle-box').removeClass('color1');
+		}
+		else {
+			$('.toggle-box').addClass('color1');
+			$('.toggle-box').removeClass('color2');
+		}
+	}
+	else {
+		if ($('.toggle-box').hasClass('color1')) {
+			$('.toggle-box').addClass('color3');
+			$('.toggle-box').removeClass('color1');
+		}
+		else {
+			$('.toggle-box').addClass('color1');
+			$('.toggle-box').removeClass('color3');
+		}
+	}
+}
+
+function pageTabClicked() {
+	cssString = $('#css-editor').text();
+	console.log(cssString);
+	console.log(cssString.indexOf('yellow'));
+	if (cssString.indexOf('yellow') != -1) {
+		$('.toggle-box').addClass('color-scheme-2');
+		$('.toggle-box').removeClass('color-scheme-1');
+	}
+	else {
+		$('.toggle-box').addClass('color-scheme-1');
+		$('.toggle-box').removeClass('color-scheme-2');
+	}
 }
